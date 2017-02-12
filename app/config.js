e3b391a1abdc6b4cc92bb27960652dcc608057cd
Renato Cardoso <re2005@gmail.com>
2017-02-12 19:47:27 +0100

    refactor(config): include constants

--- app/config.js
+++ app/config.js
@@ -1,11 +1,17 @@
 /**
  * Created on 05/05/16.
- * @author Renato  Cardoso @re2005
+ * @author Renato  Cardoso <re2005@gmail.com>
  */
 
+"use strict";
+
 var config = {
-    filename: '_icon-fonts.css',    // Filename
-    folder:'./css/'                 // Destination folder
-}
+    FILE_NAME: '_icon-fonts.css',
+    DESTINATION_FOLDER: './css/',
+    HOST: 'http://fontello.com',
+    CONFIG_JSON: './config.json',
+    CONFIG_JSON_BACKUP : './config.bkp.json',
+    SVG_SRC: '../svg'
+};
 
 module.exports = config;
